584708f2cae1986b65539ccc83d18c748ac9897a
topyonostore <topyonostore@gmail.com>
2026-09-07 22:34:10 +0530

    Delete Diwa.js

--- Diwa.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('a[href^="#"]').forEach(a=>{
-  a.onclick=e=>{
-    let x=document.querySelector(a.getAttribute("href"));
-    if(x){e.preventDefault();x.scrollIntoView({behavior:"smooth"})}
-  };
-});
